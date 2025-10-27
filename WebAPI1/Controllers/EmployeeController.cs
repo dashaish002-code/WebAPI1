da5ca4a2e29e2cfa45f390f2db2b68a1046ad44e
aishw <aishw@DESKTOP-TJ1EOS6>
2025-10-27 17:45:43 +0530

    Add project files.

---
+++ WebAPI1/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI1.Data;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetAll()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetById(int id)
+        {
+            var emp = await _context.Employees.FindAsync(id);
+            if (emp == null) return NotFound();
+            return emp;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Create(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = employee.Id }, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Employee employee)
+        {
+            if (id != employee.Id) return BadRequest();
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var emp = await _context.Employees.FindAsync(id);
+            if (emp == null) return NotFound();
+            _context.Employees.Remove(emp);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ WebAPI1/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI1.Models;
+
+namespace WebAPI1.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
